0cdcd36fb27b69bf53b3249ebee68a28060463a3
Alejandro-ADS <bryansandoval@adsecuador.com>
2022-10-21 12:23:39 -0500

    [FIX] type_pay_imporpalac: precommit

--- type_pay_imporpalac/models/sales_imporpalac.py
+++ type_pay_imporpalac/models/sales_imporpalac.py
@@ -1,5 +1,4 @@
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -16,25 +15,33 @@ class SaleOrder(models.Model):
         ],
         default="cash",
         readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}
+        states={"draft": [("readonly", False)], "sent": [("readonly", False)]},
     )
-    tax_credit_card = fields.Monetary(string='Tax credit card', store=True, readonly=True)
+    tax_credit_card = fields.Monetary(
+        string="Tax credit card", store=True, readonly=True
+    )
 
     @api.onchange("payment_method")
     def _onchange_payment_method(self):
         if self.payment_method:
             self._amount_all()
-    
+
     def _amount_all(self):
         res = super()._amount_all()
         for order in self:
-            if self.payment_method == 'credit_card':
-                percent_tax_credit_card = self.env['ir.config_parameter'].sudo().get_param('tax_credit_card') 
+            if self.payment_method == "credit_card":
+                percent_tax_credit_card = (
+                    self.env["ir.config_parameter"].sudo().get_param("tax_credit_card")
+                )
                 if percent_tax_credit_card:
-                    self.tax_credit_card = (self.amount_total*float(percent_tax_credit_card))/100
-                    order.update({
-                        'amount_total': self.amount_total+self.tax_credit_card,
-                    })
+                    self.tax_credit_card = (
+                        self.amount_total * float(percent_tax_credit_card)
+                    ) / 100
+                    order.update(
+                        {
+                            "amount_total": self.amount_total + self.tax_credit_card,
+                        }
+                    )
             else:
                 self.tax_credit_card = 0.00
         return res
