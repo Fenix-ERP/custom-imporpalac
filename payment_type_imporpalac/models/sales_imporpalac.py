8e7de9c6e825c49ee6343e9cd4a0326acd5de042
Alejandro-ADS <bryansandoval@adsecuador.com>
2022-10-25 12:26:23 -0500

    [ADD] pyment_type_imporpalac: to show tax credit card in invoice

--- payment_type_imporpalac/models/sales_imporpalac.py
+++ payment_type_imporpalac/models/sales_imporpalac.py
@@ -28,26 +28,50 @@ class SaleOrder(models.Model):
     def _amount_all(self):
         res = super()._amount_all()
         for order in self:
-            if self.payment_method.apply_tax:
-                self.apply_tax = True
-                self.tax_credit_card = (
-                    self.amount_total * self.payment_method.tax_percentage
+            if order.payment_method.apply_tax:
+                order.apply_tax = True
+                order.tax_credit_card = (
+                    order.amount_total * order.payment_method.tax_percentage
                 ) / 100
                 order.update(
                     {
-                        "amount_total": self.amount_total + self.tax_credit_card,
+                        "amount_total": order.amount_total + order.tax_credit_card,
                     }
                 )
             else:
-                self.tax_credit_card = 0.00
-                self.apply_tax = False
+                order.tax_credit_card = 0.00
+                order.apply_tax = False
         return res
+    
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals['apply_tax'] = self.apply_tax
+        invoice_vals['tax_credit_card'] = self.tax_credit_card
+        return invoice_vals
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = 'account.move'   
 
+    apply_tax  = fields.Boolean(
+        string="Apply Tax", 
+        help="Apply tax to the form of payment", 
+        default=False
+    )
     tax_credit_card = fields.Monetary(
         string="Tax credit card", store=True, readonly=True
     )
 
+    def _compute_amount(self):
+        res = super()._compute_amount()
+        for move in self:
+            if move.apply_tax:
+                move.update(
+                    {
+                        "amount_total": move.amount_total + move.tax_credit_card,
+                    }
+                )
+            else:
+                move.tax_credit_card = 0.00
+                move.apply_tax = False
+        return res
