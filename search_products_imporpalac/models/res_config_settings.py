f5926bc553c60b3fc1a0bcb33204e166fe90293a
Alejandro-ADS <bryansandoval@adsecuador.com>
2022-11-29 16:33:42 -0500

    [FIX]search_products_imporpalac: Fix view settings

--- search_products_imporpalac/models/res_config_settings.py
+++ search_products_imporpalac/models/res_config_settings.py
@@ -5,13 +5,13 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     active_search_button = fields.Boolean(
-        string="Active search button",
+        string="Show search product button",
         help="Activate summary view of products and warehouses",
         config_parameter="sale.active_search_button",
     )
     show_price_with_vat = fields.Boolean(
-        string="Show price with vat",
-        help="show the price with VAT in the summary view of products and warehouses",
+        string="Show sales price with vat",
+        help="show the sales price with VAT in the summary view of products and warehouses",
         config_parameter="sale.show_price_with_vat",
     )
 
